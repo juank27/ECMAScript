60cb53a1a9b98672748df2b046d86c2ed484bcb0
juank27 <juankruiz27@gmail.com>
2022-07-07 12:39:52 -0500

    es7

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+//includes
+let numbers = [1,2,3,4,5,6,7];
+if (numbers.includes(7)) {
+	console.log('si esta el valor 7');
+}else{
+	console.log('no se encuetra');
+}
+
+//Exponente
+let base = 4;
+let exponent = 2;
+let result = base ** exponent;
+console.log(result); //-> 16
