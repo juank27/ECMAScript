4f76714b7181c77a354dad37d155d3781f100e50
juank27 <juankruiz27@gmail.com>
2022-07-07 15:21:40 -0500

    Async - await

--- src/es8/index.js
+++ src/es8/index.js
@@ -23,3 +23,30 @@ console.log(values);
 const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, ' -----'));
+
+//Async-Await
+const helloWorld = () => {
+	return new Promise((resolve, reject) =>{
+		(true)
+			? setTimeout(() => resolve('hello world'), 3000)
+			: reject(new Error('test error'));
+	})
+}
+
+//como usarla
+const helloAsync = async () => {
+	const hello = await helloWorld();
+	console.log(hello);
+}
+//haciendo el llamado
+helloAsync(); //-> hello world
+
+//otra forma de manejar el async
+const anotherFunction = async () => {
+	try {
+		const hello = await helloWorld();
+		console.log();
+	} catch (error) {
+		console.log(error);
+	}
+}
