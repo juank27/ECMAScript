e23a99c6db499cb5081ba25e9663d8f92f4e75bf
juank27 <juankruiz27@gmail.com>
2022-07-07 19:58:16 -0500

    es11 Otras caracteristicas

--- src/es11/index.js
+++ src/es11/index.js
@@ -7,3 +7,39 @@ button.addEventListener("click", async function() {
 	//llama la funcion del archivo file.js
 	module.hello();
 });
+
+//promise.allSettle
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolv 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+	.then(response => console.log(response));
+
+//Operador Nullish Coalescing
+const usuario1 = {}
+const nombre1 = usuario1.name ?? "Andres"
+
+const usuario2 = { name: "Juan" }
+const nombre2 = usuario2.name ?? "Andres"
+
+console.log(nombre1) // 'Andres'
+console.log(nombre2) // 'Juan'
+//ejemplo sensillo
+const fooo = null ?? 'default string';
+console.log(fooo); //-> 'default string'
+const fooo2 = 'Hola' ?? 'default string';
+console.log(fooo2); //-> 'Hola'
+
+//encadenamiento opcional
+const user = {};
+//opcion mal hecha
+console.log(user.profile.name); //-> error
+//la aplicacion no para y sigue ejecutando el codigo
+console.log(user?.profile?.name); //-> undefined
+
+if (user?.profile?.name) {
+	console.log('name');
+}else{
+	console.log('fail');
+}
